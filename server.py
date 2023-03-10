3e295e628f549678f340d036bb17db80eb2c043f
charith-pathi <c_pathirathna@hotmail.com>
2023-03-09 18:16:32 -0700

    Made changes

--- server.py
+++ server.py
@@ -74,7 +74,7 @@ def flask_post_json():
 @app.route("/")
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return redirect("/static/index.html")
+    return redirect("/static/index.html", code=302)
 
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
